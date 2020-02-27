8f584cdf4a293b4569f5ba717de3e747e40172a8
pjanupl <48182321+pjanupl@users.noreply.github.com>
2020-02-27 14:23:07 +0100

    pierwsza rewizja

---
+++ tester.py
@@ -0,0 +1,11 @@
+# (pjanu)
+# 24.06.2018 r.
+# 
+# Python Programming for the Absolute Beginner, 3rd Edition
+# Michael Dawson
+# page 29./ex. 3. (polish edition)
+# The program displays the favorite quote and in the next line of its author
+
+print("\"(Poza tym uważam, że) Kartaginę należy zniszczyć\"")
+print("Kato Starszy")
+input()
